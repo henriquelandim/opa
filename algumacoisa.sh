1cac655c6daea73c252d42f93cb84418e656c059
henriquelandim <henriquelandim@outlook.com.br>
2017-10-27 20:03:20 -0200

    correção dos erros de ortografia

---
+++ algumacoisa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#programa para calcular dois numeros e fazer as quatros opecoes 
+clear 
+echo "----------------------------------------------"
+echo "-                                            -"
+echo "-      Programa de Calculos: 4 operações     -"
+echo "-                                            -"
+echo "----------------------------------------------"
+echo 
+echo "Digite um numero"
+read n1
+echo "Digite outro numero"
+read n2
+echo "O resultado da soma é: $((n1+n2))"
+echo "O resultado da subtracao é: $((n1-n2))"
+echo "O resultado da mutiplio é: $((n1*n2))"
+echo "O resultado da divisao é: $((n1/n2))"
+echo "Você é burro" 
+
+
